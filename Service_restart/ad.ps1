33cee108652c63306b6f012bcd4d43da1c7816f3
kahu5 <47630569+kahu5@users.noreply.github.com>
2023-12-19 10:57:02 -0500

    Update ad.ps1
    eliminating the chance duplicate instances may be created when wildcards are used

--- Service_restart/ad.ps1
+++ Service_restart/ad.ps1
@@ -1,11 +1,15 @@
 $hostname = '##HOSTNAME##'
 $prop = '##Service_restart.services##'
 $splitprops = $prop.split(",| ",[System.StringSplitOptions]::RemoveEmptyEntries)
+$processedServices = @{} # Hashtable to store processed services
 Foreach ($i in $splitprops){
   $services = get-service -name $i -ComputerName $hostname
   if ($services){
     Foreach ($service in $services) {
-      "$($service.Name)##$($service.DisplayName)"
+      if (-not $processedServices.ContainsKey($service.Name)) { # Check if service name is not in hashtable
+        "$($service.Name)##$($service.DisplayName)"
+        $processedServices[$service.Name] = $true # Add service name to hashtable
+      }
     }
   } else {
     Write-Host "Service '$i' not found."
