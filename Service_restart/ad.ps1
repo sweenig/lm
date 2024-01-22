a3524dbbf758d357bc10ca7feef040cb0f9cec33
kahu5 <47630569+kahu5@users.noreply.github.com>
2024-01-22 15:56:14 -0500

    Update ad.ps1
    Added check for WMI polling where possible to reduce failures, falls back to Get-Service command

--- Service_restart/ad.ps1
+++ Service_restart/ad.ps1
@@ -1,10 +1,24 @@
 $hostname = '##HOSTNAME##'
+$username = '##wmi.user##'
+$password = '##wmi.pass##' | ConvertTo-SecureString -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential($username, $password)
 $prop = '##Service_restart.services##'
+
 $splitprops = $prop.split(",| ",[System.StringSplitOptions]::RemoveEmptyEntries)
+$processedServices = @{} # Hashtable to store processed services
+
 Foreach ($i in $splitprops){
-  $service = get-service -name $i -ComputerName $hostname
-  if ($service){
-    "$($service.Name)##$($service.DisplayName)"
+    $services = Get-WmiObject -Class Win32_Service -Filter "Name='$i'" -ComputerName $hostname -Credential $credential
+  if (!$services){
+      $services = Get-Service -name $i -ComputerName $hostname
+  }
+  if ($services){
+    Foreach ($service in $services) {
+      if (-not $processedServices.ContainsKey($service.Name)) { # Check if service name is not in hashtable
+        "$($service.Name)##$($service.DisplayName)"
+        $processedServices[$service.Name] = $true # Add service name to hashtable
+      }
+    }
   } else {
     Write-Host "Service '$i' not found."
   }
