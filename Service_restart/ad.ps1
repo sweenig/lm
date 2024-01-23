b89bbfcee97b5c0fbfea37aab88f39f0b1f49f6f
kahu5 <47630569+kahu5@users.noreply.github.com>
2024-01-23 08:37:51 -0500

    Update ad.ps1
    improved and optimized the script for WMI and checking for failures

--- Service_restart/ad.ps1
+++ Service_restart/ad.ps1
@@ -1,26 +1,32 @@
 $hostname = '##HOSTNAME##'
 $username = '##wmi.user##'
-$password = '##wmi.pass##' | ConvertTo-SecureString -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential($username, $password)
+$password = '##wmi.pass##'
+if($username -and $password){
+  $securePassword = $password | ConvertTo-SecureString -AsPlainText -Force
+  $credential = New-Object System.Management.Automation.PSCredential($username, $securePassword)
+}
 $prop = '##Service_restart.services##'
 
 $splitprops = $prop.split(",| ",[System.StringSplitOptions]::RemoveEmptyEntries)
 $processedServices = @{} # Hashtable to store processed services
 
+# Get all services once
+$allServices = Get-WmiObject -Class Win32_Service -ComputerName $hostname -Credential $credential
+
 Foreach ($i in $splitprops){
-    $services = Get-WmiObject -Class Win32_Service -Filter "Name='$i'" -ComputerName $hostname -Credential $credential
-  if (!$services){
-      $services = Get-Service -name $i -ComputerName $hostname
-  }
-  if ($services){
-    Foreach ($service in $services) {
-      if (-not $processedServices.ContainsKey($service.Name)) { # Check if service name is not in hashtable
-        "$($service.Name)##$($service.DisplayName)"
-        $processedServices[$service.Name] = $true # Add service name to hashtable
-      }
+    Try {
+        $services = $allServices | Where-Object { $_.Name -eq $i }
+        if (-not $services) {
+            $services = Get-Service -name $i -ComputerName $hostname
+        }
+        Foreach ($service in $services) {
+            if (-not $processedServices.ContainsKey($service.Name)) { # Check if service name is not in hashtable
+                "$($service.Name)##$($service.DisplayName)##Windows Service-$($service.Name)"
+                $processedServices[$service.Name] = $true # Add service name to hashtable
+            }
+        }
+    } Catch {
+        Write-Host "Error occurred while processing service '$i': $_"
     }
-  } else {
-    Write-Host "Service '$i' not found."
-  }
 }
 exit 0
