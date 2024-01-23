aec385813fa7828a1f11ab9dae848839702a5cd0
kahu5 <47630569+kahu5@users.noreply.github.com>
2024-01-23 08:39:40 -0500

    Update collect.ps1
    added checking for WMI and optimized script

--- Service_restart/collect.ps1
+++ Service_restart/collect.ps1
@@ -1,37 +1,52 @@
 $hostname = "##HOSTNAME##"
+$username = '##wmi.user##'
+$password = '##wmi.pass##'
+$credential = $null
+if($username -and $password){
+  $securePassword = $password | ConvertTo-SecureString -AsPlainText -Force
+  $credential = New-Object System.Management.Automation.PSCredential($username, $securePassword)
+}
 $service = "##WILDVALUE##"
-$wait = 5 # seconds to wait
+$wait = 15 # seconds to wait
 
-#Stop-Service -InputObject $(Get-Service -Computer $hostname -Name $service)  # testing only
-#Start-Sleep 4  # testing only
+# Define a function to get service status
+function Get-ServiceStatus($service, $hostname, $credential) {
+  $service_status = (Get-Service -Name $service -ComputerName $hostname -ErrorAction SilentlyContinue).Status
+  if (-not $service_status -and $credential) {
+    $service_status = (Get-WmiObject -Class Win32_Service -Filter "Name='$service'" -ComputerName $hostname -Credential $credential).State
+  }
+  return $service_status
+}
 
-$service_status = (Get-Service -Name $service -ComputerName $hostname).Status
+# Try to get the service status
+$service_status = Get-ServiceStatus -service $service -hostname $hostname -credential $credential
+
 Write-Host "$service is $service_status on $hostname"
 
-if ((get-service -name $service -ComputerName $hostname).Status -eq "Running") {
-  write-host "result_code: 1 (running)"
+if ($service_status -eq "Running") {
+  Write-Host "result_code: 1 (running)"
 } else {
   Write-Host "not running so i will start then wait $wait seconds and test"
-  Start-Service -InputObject $(Get-Service -Computer $hostname -Name $service)
+  Start-Service -InputObject $(Get-Service -Computer $hostname -Name $service -ErrorAction SilentlyContinue)
   Start-Sleep $wait
 
-  #Stop-Service -InputObject $(Get-Service -Computer $hostname -Name $service) # testing only
-  #Start-Sleep 4 # testing only
+  # Check the service status again
+  $service_status = Get-ServiceStatus -service $service -hostname $hostname -credential $credential
 
-  if ((get-service -name $service -ComputerName $hostname).Status  -eq "Running") {
-        write-host "result_code: 2 (first restart worked)"
+  if ($service_status -eq "Running") {
+    Write-Host "result_code: 2 (first restart worked)"
+  } else {
+    Write-Host "first restart failed so i will start second time"
+    Start-Service -InputObject $(Get-Service -Computer $hostname -Name $service -ErrorAction SilentlyContinue)
+    Start-Sleep $wait
+
+    # Check the service status again
+    $service_status = Get-ServiceStatus -service $service -hostname $hostname -credential $credential
+
+    if ($service_status -eq "Running") {
+      Write-Host "result_code: 3 (second restart worked)"
     } else {
-        Write-Host "first restart failed so i will start second time"
-        Start-Service -InputObject $(Get-Service -Computer $hostname -Name $service)
-        Start-Sleep $wait
-
-        #Stop-Service -InputObject $(Get-Service -Computer $hostname -Name $service) # testing only
-        #Start-Sleep 3 # testing only
-
-        if ((get-service -name $service -ComputerName $hostname).Status  -eq "Running") {
-            Write-Host "result_code: 3 (second restart worked)"
-        } else {
-            Write-Host "result_code: 4 (second restart failed so probably trigger alert)"
-        } # end of else line 21
-    } # end of else line 15
-} # end of line 9
+      Write-Host "result_code: 4 (second restart failed so probably trigger alert)"
+    }
+  }
+}
